325dafcbe3b21269e9ffa36317e70e06cf6cc0f4
VollRahm <39211361+VollRahm@users.noreply.github.com>
2018-10-16 20:05:00 +0200

    Added source

---
+++ ForceAltF4/mainFrm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ForceAltF4
+{
+    partial class mainFrm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainFrm));
+            this.contextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.activeStripItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.autostartItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenu
+            // 
+            this.contextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitItem,
+            this.activeStripItem,
+            this.autostartItem});
+            this.contextMenu.Name = "contextMenu";
+            this.contextMenu.Size = new System.Drawing.Size(181, 92);
+            // 
+            // exitItem
+            // 
+            this.exitItem.Name = "exitItem";
+            this.exitItem.Size = new System.Drawing.Size(123, 22);
+            this.exitItem.Text = "Exit";
+            this.exitItem.Click += new System.EventHandler(this.exitItem_Click);
+            // 
+            // activeStripItem
+            // 
+            this.activeStripItem.Checked = true;
+            this.activeStripItem.CheckOnClick = true;
+            this.activeStripItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.activeStripItem.Name = "activeStripItem";
+            this.activeStripItem.Size = new System.Drawing.Size(123, 22);
+            this.activeStripItem.Text = "Active";
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "ForceAltF4";
+            this.notifyIcon.BalloonTipTitle = "lel";
+            this.notifyIcon.ContextMenuStrip = this.contextMenu;
+            this.notifyIcon.Text = "ForceAltF4";
+            this.notifyIcon.Visible = true;
+            // 
+            // autostartItem
+            // 
+            this.autostartItem.CheckOnClick = true;
+            this.autostartItem.Name = "autostartItem";
+            this.autostartItem.Size = new System.Drawing.Size(180, 22);
+            this.autostartItem.Text = "Autostart";
+            this.autostartItem.Click += new System.EventHandler(this.autostartItem_Click);
+            // 
+            // mainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 177);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainFrm";
+            this.ShowInTaskbar = false;
+            this.Text = "ForceAltF4";
+            this.contextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ToolStripMenuItem activeStripItem;
+        private System.Windows.Forms.ToolStripMenuItem autostartItem;
+    }
+}
+
---
+++ ForceAltF4/mainFrm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using MouseKeyboardLibrary;
+
+namespace ForceAltF4
+{
+    
+    public partial class mainFrm : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        KeyboardHook keyboard = new KeyboardHook();
+
+        bool autostart;
+
+        public mainFrm()
+        {
+            InitializeComponent();
+            keyboard.KeyDown += new KeyEventHandler(kbPress);
+            keyboard.Start();
+            notifyIcon.Icon = this.Icon;
+            this.Hide();
+            if (File.Exists("config.altf4"))
+            {
+                StreamReader reader = new StreamReader("config.altf4");
+                if (reader.ReadLine() == "1")
+                {
+                    autostart = true;
+                    autostartItem.Checked = true;
+                    autostartItem.CheckState = CheckState.Checked;
+                }
+                else if (reader.ReadLine() == "0")
+                {
+                    autostart = false;
+                    autostartItem.Checked = false;
+                    autostartItem.CheckState = CheckState.Unchecked;
+                }
+                
+                reader.Close();
+                
+            }
+            else
+            {
+                
+                StreamWriter writer = new StreamWriter("config.altf4");
+                writer.WriteLine("1");
+                writer.Close();
+                autostart = true;
+                RegistryKey rk = Registry.CurrentUser.OpenSubKey
+           ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                rk.SetValue("ForceAltF4", Application.ExecutablePath);
+                autostartItem.Checked = true;
+                autostartItem.CheckState = CheckState.Checked;
+
+                notifyIcon.ShowBalloonTip(1000, "ForceAltF4", "App running in Background. Autostart set. Click on the Icon to disable it.", ToolTipIcon.Info);
+                notifyIcon.ShowBalloonTip(2000, "ForceAltF4", "Press Ctrl + Alt + F4 to kill the current process.", ToolTipIcon.Info);
+            }
+        }
+
+        private void kbPress(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.F4 && (ModifierKeys & Keys.Control) == Keys.Control && (ModifierKeys & Keys.Alt) == Keys.Alt) && activeStripItem.Checked)
+            {
+                IntPtr hwnd = GetForegroundWindow();
+                uint pid;
+                GetWindowThreadProcessId(hwnd, out pid);
+                Process p = Process.GetProcessById((int)pid);
+                p.Kill();
+                notifyIcon.ShowBalloonTip(500, "ForceAltF4", "Process Killed", ToolTipIcon.Info);
+
+
+
+            }
+        }
+        Process GetActiveProcessFileName()
+        {
+            IntPtr hwnd = GetForegroundWindow();
+            uint pid;
+            GetWindowThreadProcessId(hwnd, out pid);
+            Process p = Process.GetProcessById((int)pid);
+            return p;
+            
+        }
+
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+            Opacity = 0;
+
+            base.OnLoad(e);
+        }
+
+        private void autostartItem_Click(object sender, EventArgs e)
+        {
+            if (autostartItem.Checked && !autostart)
+            {
+                autostart = true;
+                RegistryKey rk = Registry.CurrentUser.OpenSubKey
+           ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                rk.SetValue("ForceAltF4", Application.ExecutablePath);
+                File.Delete("config.altf4");
+                StreamWriter writer = new StreamWriter("config.altf4");
+                writer.WriteLine("1");
+                writer.Close();
+            }
+            else if(!autostartItem.Checked)
+            {
+                RegistryKey rk = Registry.CurrentUser.OpenSubKey
+           ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                rk.DeleteValue("ForceAltF4", false);
+                autostart = false;
+                File.Delete("config.altf4");
+                StreamWriter writer = new StreamWriter("config.altf4");
+                writer.WriteLine("0");
+                writer.Close();
+            }
+        }
+    }
+}
